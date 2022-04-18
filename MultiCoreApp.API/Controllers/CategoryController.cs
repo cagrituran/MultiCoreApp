f73c1bdb093b5f999d9095d1d5bb54306cd29b8a
cagrituran <cagri.turann@hotmail.com>
2022-04-18 19:36:26 +0300

    Auto-Mapping and Database Transfer Objects has been built for this project and Category,Product Controllers has been finished

--- MultiCoreApp.API/Controllers/CategoryController.cs
+++ MultiCoreApp.API/Controllers/CategoryController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MultiCoreApp.API.DTOs;
 using MultiCoreApp.Core.IntService;
+using MultiCoreApp.Core.Models;
 
 namespace MultiCoreApp.API.Controllers
 {
@@ -9,22 +12,57 @@ namespace MultiCoreApp.API.Controllers
     public class CategoryController : ControllerBase
     {
         private ICategoryService _catService;
-        public CategoryController(ICategoryService catService)
+        private IMapper _mapper;
+        public CategoryController(ICategoryService catService, IMapper mapper)
         {
             _catService = catService;
+            _mapper = mapper;
 
         }
         [HttpGet]//Select işlemleri için api kullanılan keywordu
         public async Task<IActionResult> GetAll()
         {
             var cat = await _catService.GetAllAsync();
-            return Ok(cat);
+            //return Ok(cat);
+            return Ok(_mapper.Map<IEnumerable<CategoryDto>>(cat));
         }
         [HttpGet("{id:Guid}")]
         public async Task<IActionResult> GetById(Guid id)
         {
             var cat = await _catService.GetByIdAsync(id);
-            return Ok(cat);
+            return Ok(_mapper.Map<CategoryDto>(cat));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(CategoryDto catDto)
+        {
+            var newCat = await _catService.AddAsync(_mapper.Map<Category>(catDto));
+            return Created(string.Empty, _mapper.Map<CategoryDto>(newCat));
+        }
+        [HttpPut]
+        public IActionResult Update(CategoryDto catDto)
+        {
+            var cat = _catService.Update(_mapper.Map<Category>(catDto));
+            return NoContent();
+        }
+        [HttpDelete("{id:guid}")]
+        public IActionResult Remove(Guid id)
+        {
+            var cat = _catService.GetByIdAsync(id).Result; // Result senkron yapılarda asenkron metodu calıstırdıgı için hatayı engellemek için
+            _catService.Remove(cat);
+            return NoContent();
+        }
+        [HttpDelete("{name}")]
+        public IActionResult RemoveByName(string name)
+        {
+            var cat = _catService.Where(s => s.Name == name).Result;
+            _catService.RemoveRange(cat);
+            return NoContent();
+        }
+        [HttpGet("{id:guid}/products")]
+        public async Task<IActionResult> GetWithProductById(Guid id)
+        {
+            var cat = await _catService.GetWithProductByIdAsync(id);
+            return Ok(_mapper.Map<CategoryWithProductsDto>(cat));
         }
     }
 }
---
+++ MultiCoreApp.API/DTOs/CategoryDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiCoreApp.API.DTOs
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; } /*= Guid.NewGuid();*/
+        [Required(ErrorMessage = "{0} alani zorunludur.")]
+        public string Name { get; set; }
+    }
+}
