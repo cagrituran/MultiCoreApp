7983f1cb497dc350c6800709f52210041ca4dee8
cagrituran <cagri.turann@hotmail.com>
2022-05-23 18:30:42 +0300

    api connection with react-native

---
+++ multireactnative/App.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable eol-last */
+import React from 'react';
+import Router from './src/Router';
+
+export default function App(){
+  return <Router/>;
+}
